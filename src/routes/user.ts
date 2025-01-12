754f11bdf0c6b9e165b41530076ef2a7b8f620ac
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2025-01-12 21:22:24 +0700

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,10 +3,12 @@ import {
   signId,
   verifyEmail,
 } from "../controllers/user";
+import { sendRes } from "../controllers/setup";
 
 const router = express.Router();
 
 
 router.post("/signId/", signId); //
 router.post("/verifyEmail/", verifyEmail); //
+router.get('/test/',(req,res)=>{sendRes(res,true)})
 export default router;
