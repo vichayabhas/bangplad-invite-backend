e4f738644734b90caa53d98ee411ef6fcefafc9d
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2025-01-12 23:02:15 +0700

    Revert "Update index.ts"
    This reverts commit e22b5bd3a04f9be707ccc8803c3a947f22e3df5f.

--- index.ts
+++ index.ts
@@ -1,58 +1,14 @@
-// import express from "express";
-// import cors from "cors";
-
-// import user from "./src/routes/user";
-// import cookieParser from "cookie-parser";
-
-
-
-
-// const app = express();
-// app.use(express.json());
-// app.use(cookieParser());
-// //Body parser
-// app.use(express.json());
-
-// app.use(cors());
-// app.use("", user);
-
-// // const PORT = process.env.PORT || 5000;
-// // const server = app.listen(PORT, () =>
-// //   console.log(
-// //     "Server running in ",
-// //     process.env.NODE_ENV,
-// //     " mode on port ",
-// //     PORT
-// //   )
-// // );
-
-// // process.on("unhandledRejection", (err: Error) => {
-// //   console.log(`Error: ${err.message}`);
-// //   server.close(() => process.exit(1));
-// // });
-// export default app;
-// //console.log('jjjjjjjjjjjjjjjjjjjbutfyiknjjjjj')
+import express from "express";
 import cors from "cors";
 
 import user from "./src/routes/user";
 import cookieParser from "cookie-parser";
 
-import express, { Request, Response } from "express";
+
+
 
 const app = express();
 app.use(express.json());
-
-// get post routes
-app.get("/post", (req: Request, res:Response) => {
-  res.status(200).json({ message: "post routes" });
-});
-
-// root routes
-app.get("/", (req: Request, res:Response) => {
-  res.status(200).json({ message: "Hello World" });
-});
-
-
 app.use(cookieParser());
 //Body parser
 app.use(express.json());
@@ -60,6 +16,19 @@ app.use(express.json());
 app.use(cors());
 app.use("", user);
 
+// const PORT = process.env.PORT || 5000;
+// const server = app.listen(PORT, () =>
+//   console.log(
+//     "Server running in ",
+//     process.env.NODE_ENV,
+//     " mode on port ",
+//     PORT
+//   )
+// );
 
-
+// process.on("unhandledRejection", (err: Error) => {
+//   console.log(`Error: ${err.message}`);
+//   server.close(() => process.exit(1));
+// });
 export default app;
+//console.log('jjjjjjjjjjjjjjjjjjjbutfyiknjjjjj')
