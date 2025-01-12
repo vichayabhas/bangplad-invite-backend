a645d1e7636f780163043789fec5e7e75e2ac823
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2025-01-12 21:26:10 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors());
-app.use("", user);
+app.use("/user", user);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
