fc595b010525e8be6513c51acf7c3b38c358ac77
vichayabhas <156632600+vichayabhas@users.noreply.github.com>
2025-01-13 14:28:08 +0700

    Revert "Update index.ts"
    This reverts commit af218aa5bed27b97f4f2854e7581ed1c275a3870.

--- index.ts
+++ index.ts
@@ -16,19 +16,19 @@ app.use(express.json());
 app.use(cors());
 app.use("", user);
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () =>
-  console.log(
-    "Server running in ",
-    process.env.NODE_ENV,
-    " mode on port ",
-    PORT
-  )
-);
+// const PORT = process.env.PORT || 5000;
+// const server = app.listen(PORT, () =>
+//   console.log(
+//     "Server running in ",
+//     process.env.NODE_ENV,
+//     " mode on port ",
+//     PORT
+//   )
+// );
 
-process.on("unhandledRejection", (err: Error) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
+// process.on("unhandledRejection", (err: Error) => {
+//   console.log(`Error: ${err.message}`);
+//   server.close(() => process.exit(1));
+// });
 export default app;
 //console.log('jjjjjjjjjjjjjjjjjjjbutfyiknjjjjj')
